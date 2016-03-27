0f230892336ddee07b9ecf63df4967b346f89093
Mindi Torrey <minditorrey@me.com>
2016-03-27 19:54:38 -0400

    Added all files for todo list
    index.html, todos.js, todolist.css, jquery

---
+++ todos.js
@@ -0,0 +1,26 @@
+//Check of todos by clicking
+$('ul').on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+//click on X to delete todo
+$('ul').on("click", 'span', function(event) {
+	$(this).parent().fadeOut(500, function (){
+		$(this).remove();
+});
+	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13) {
+		//grab value from inuput
+		var todoText = $(this).val();
+		//clear out input
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function () {
+	$("input[type='text'").fadeToggle();
+})
